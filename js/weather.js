f34707795bc1dcd53a061638fc130f5538c9c191
Yuren Ju <yurenju@gmail.com>
2012-09-27 00:04:50 +0800

    Update District with geo-location

--- js/weather.js
+++ js/weather.js
@@ -3,6 +3,60 @@
 var Weather = {
 	svg: null,
 	pt: null,
+	geocoder: null,
+
+	init: function weather_init() {
+		this.initEvent();
+		this.initGeo();
+	},
+
+	initGeo: function weather_initGeo() {
+		this.geocoder = new google.maps.Geocoder();
+		var that = this;
+		var geocoder = this.geocoder;
+		if (navigator.geolocation) {
+			//navigator.geolocation.getCurrentPosition(this.getPosSuccess, null);
+			var that = this;
+			navigator.geolocation.getCurrentPosition(function (position) {
+				var lat = position.coords.latitude;
+	    		var lng = position.coords.longitude;
+	    		that.fillDistrict(lat, lng);
+			});
+		}
+	},
+
+	fillDistrict: function weather_fillDistrict(lat, lng) {
+		var latlng = new google.maps.LatLng(lat, lng);
+		var that = this;
+		this.geocoder.geocode({'latLng': latlng}, function(results, status) {
+  			if (status == google.maps.GeocoderStatus.OK) {
+  				console.log(results)
+    			if (results[1]) {
+     				//formatted address
+     				//alert(results[0].formatted_address)
+     				var district;
+    				//find country name
+         			for (var i=0; i<results[0].address_components.length; i++) {
+        				for (var b=0;b<results[0].address_components[i].types.length;b++) {
+			                if (results[0].address_components[i].types[b] == "locality") {
+			                    district= results[0].address_components[i];
+			                    break;
+			                }
+			            }
+    				}
+			        that.updateDistrict(district.long_name);
+    			} else {
+      				alert("No results found");
+    			}
+  			} else {
+    			alert("Geocoder failed due to: " + status);
+  			}
+		});
+	},
+
+	updateDistrict: function weather_updateDistrict(name) {
+		document.getElementById('district').innerHTML = name;
+	},
 
 	initEvent: function weather_initEvent() {
 		this.svg = document.getElementsByTagName('svg')[0];
@@ -19,10 +73,6 @@ var Weather = {
 	},
 
 	handleEvent: function weather_handleEvent(evt) {
-		this.handleMove(evt);
-	},
-
-	handleMove: function weather_handleMove(evt) {
 		var line = document.getElementById('selected-line');
 		var current = this.cursorPoint(evt);
 		line.setAttribute('transform', 'translate(' + current.x + ',0)');
@@ -31,5 +81,5 @@ var Weather = {
 
 window.addEventListener('load', function weatLoad(evt) {
 	window.removeEventListener('load', weatLoad);
-	Weather.initEvent();
+	Weather.init();
 });
