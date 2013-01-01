52af8ec46c23ec983f3a8db12af30fc7b226549e
Yuren Ju <yurenju@gmail.com>
2013-01-01 13:16:53 +0800

    move initial Weather to setup

--- test/weather_test.js
+++ test/weather_test.js
@@ -1,24 +1,23 @@
 var jsdom = require('jsdom').jsdom;
 var assert = require('chai').assert;
 var html = require('fs').readFileSync('index.html').toString();
-var Weather = require(__dirname + '/../js/weather.js').Weather;
-var window, document;
+var window, document, Weather;
 
 suite('Weather', function() {
   setup(function() {
     document = jsdom(html);
     window = document.createWindow();
+    Weather = require(__dirname + '/../js/weather.js').Weather;
+    Weather.init(window, document);
   });
 
   test('getAllElements', function() {
-    Weather.init(window, document);
     assert.equal('temp-graph', Weather.tempGraph.id);
     assert.equal('current-temp', Weather.currentTemp.parentNode.id);
     assert.equal('high-temp-0', Weather['maxTemp0'].id);
   });
 
   test('updateWeekday', function() {
-    Weather.init(window, document);
     //2013/1/1 is Tuesday
     Weather.updateWeekday(new Date('Jan 01, 2013'));
     assert.equal(Weather.day0.textContent, 'T', 'day0 is [T]uesday');
